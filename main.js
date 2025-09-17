e8ff1ebf353c847172608c330adf452b1c6b74f5
JD <juandruiz298@gmail.com>
2025-09-17 11:49:49 +0200

    1 way IPC

--- main.js
+++ main.js
@@ -1,33 +1,31 @@
-const { app, BrowserWindow, ipcMain} = require('electron/main')
+const { app, BrowserWindow, ipcMain } = require('electron/main')
+const path = require('node:path')
 
-const path = require("node:path");
+function handleSetTitle (event, title) {
+  const webContents = event.sender
+  const win = BrowserWindow.fromWebContents(webContents)
+  win.setTitle(title)
+}
 
-const createWindow = () => {
-
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+function createWindow () {
+  const mainWindow = new BrowserWindow({
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
 
-  win.loadFile('index.html')
+  mainWindow.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
-    ipcMain.handle('ping', () => 'pong')
-  createWindow()
+  ipcMain.on('set-title', handleSetTitle)
   createWindow()
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
-app.on('window-all-closed', () => {
-  app.quit()
-  
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
 })
--- renderer.js
+++ renderer.js
@@ -1,11 +1,6 @@
-const information = document.getElementById('info')
-information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
-
-
-const func = async () => {
-    console.log('ping 1')
-    const response = await window.versions.ping()
-    console.log(response) // prints out 'pong'
-  }
-  
-  func()
+const setButton = document.getElementById('btn')
+const titleInput = document.getElementById('title')
+setButton.addEventListener('click', () => {
+  const title = titleInput.value
+  window.electronAPI.setTitle(title)
+})
