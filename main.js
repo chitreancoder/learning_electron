36063af1fb0fa1dab2d06c201b42c3ef719fd5ca
JD <juandruiz298@gmail.com>
2025-09-17 11:55:40 +0200

    2 way IPC

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-const { app, BrowserWindow, ipcMain } = require('electron/main')
+const { app, BrowserWindow, ipcMain, dialog } = require('electron/main')
 const path = require('node:path')
 
-function handleSetTitle (event, title) {
-  const webContents = event.sender
-  const win = BrowserWindow.fromWebContents(webContents)
-  win.setTitle(title)
+async function handleFileOpen () {
+  const { canceled, filePaths } = await dialog.showOpenDialog()
+  if (!canceled) {
+    return filePaths[0]
+  }
 }
 
 function createWindow () {
@@ -13,14 +14,12 @@ function createWindow () {
       preload: path.join(__dirname, 'preload.js')
     }
   })
-
   mainWindow.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
-  ipcMain.on('set-title', handleSetTitle)
+  ipcMain.handle('dialog:openFile', handleFileOpen)
   createWindow()
-
   app.on('activate', function () {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
